2612d793aaa3a076738c47d9126ac8aba210ebd6
rioles <rioles1992@gmail.com>
2023-02-16 20:04:05 +0100

    rotate_image

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -17,3 +17,11 @@ def rotate_2d_matrix(matrix):
 
         left += 1
         right -= 1
+
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    """ rotate_2d_matrix(matrix) """
+    rotate_2d_matrix(matrix)
+    print(matrix)
