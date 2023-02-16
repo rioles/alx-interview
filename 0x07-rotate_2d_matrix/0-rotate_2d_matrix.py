e81cf357388795a3ec11392f4624a6ff1a14ba7d
rioles <rioles1992@gmail.com>
2023-02-16 20:09:53 +0100

    rotate image

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
+""" Rotate 2D Matrix
+"""
+
 
 def rotate_2d_matrix(matrix):
-    left, right = 0, len(matrix[0]) - 1
-
-    while left < right:
-
-        for i in range(right - left):
-            top, bottom = left, right
-
-            top_left = matrix[top][left + i]
-
-            matrix[top][left + i] = matrix[bottom - i][left]
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            matrix[bottom][right - i] = matrix[top + i][right]
-            matrix[top + i][right] = top_left
-
-        left += 1
-        right -= 1
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
+    """
+    for x, y in enumerate(zip(*reversed(matrix))):
+        matrix[x] = list(y)
 
 
 if __name__ == '__main__':
